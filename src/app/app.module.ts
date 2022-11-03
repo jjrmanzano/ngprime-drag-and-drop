7dc308399f32f314fb88faaa7ff51de304577882
Jorge Juan Rodríguez Manzano <jorrodriguez@theworkshop.com>
2022-11-03 19:28:54 +0100

    Add a Child Component to place the Drop from the Drag&Drop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ParentComponent } from './parent/parent.component';
+import { ChildComponent } from './parent/child/child.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ParentComponent
+    ParentComponent,
+    ChildComponent
   ],
   imports: [
     BrowserModule,
